5764f03df7f651b2a06ba5017e63658f9b597df7
Thomas Neumayr <tom.neumayr@gmail.com>
2019-09-16 22:56:13 -0400

    added error to testfile

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -23,7 +23,7 @@ public class HelloTest {
 		
 		// Check for desired result
 		String s = baos.toString();
-		if (! "Hello, Jay.\n".equals(s))
+		if (! "Hllo, Jay.\n".equals(s))
 			fail("Unexpected output:\n"+s+"\n");
 	}
 	
